cc061950c0a63344098a88c758202ed804566767
yashrawal13 <120002039+yashrawal13@users.noreply.github.com>
2023-01-02 12:28:26 +0530

    yash changed

--- yash.py
+++ yash.py
@@ -2,9 +2,9 @@
 
 num1 = 1.5
 num2 = 6.3
-
+num3 = 7.7
 # Add two numbers
-sum = num1 + num2
+sum = num1 + num2 + num3
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
